38acb3405ed9173bc0d172e0cae909554c9f51cf
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-15 23:04:11 +0530

    setup express server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,29 @@
+import express from 'express'
+import { cocClient } from './coc_api'
 import { login_bot } from './discord_bot'
+import { db, toTimeStamp } from './firebase'
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(5000)
+
+app.get('/api/players', async (req, res, next) => {
+  const snapshot = await db.collection('players').get()
+  const tags = snapshot.docs.map(s => s.data().player_tag)
+  console.log(tags)
+
+  const players: any = await Promise.all(
+    tags.map(t => cocClient.playerByTag(t))
+  )
+  res.json(players)
+})
+
+app.post('/api/roasters', (req, res, next) => {
+  const response = { ...req.body, date: toTimeStamp(new Date()) }
+  db.collection('roasters').add({ response })
+  res.json(response)
+})
 
 login_bot()
-console.log('hi')
