6b54949997e14ec3729b7cac7658af28600496bc
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-18 22:43:56 +0530

    add little content

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,12 @@
 import { Box } from '@chakra-ui/react'
+import { useGetWars } from '../hooks/useGetWars'
 
-const home = () => {
-  return <Box></Box>
+const Home = () => {
+  const { data } = useGetWars()
+  if (!data) return null
+  console.log(data.map(war => new Date(war.spin_time)))
+
+  return <Box>Hello Idow!</Box>
 }
 
-export default home
+export default Home
