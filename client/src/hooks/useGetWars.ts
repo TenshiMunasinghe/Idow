95090b45e85323b50037eba448287daa1ce0cf6b
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-18 22:47:43 +0530

    initial commit

---
+++ client/src/hooks/useGetWars.ts
@@ -0,0 +1,15 @@
+import ky from 'ky'
+import { useQuery } from 'react-query'
+import { War } from '../../../server/src/utils/get_players_details'
+
+interface ClientWar extends Omit<War, 'spin_time'> {
+  spin_time: string
+}
+
+const getWars: () => Promise<ClientWar[]> = async () => {
+  return await ky.get('/api/wars').json()
+}
+
+export const useGetWars = () => {
+  return useQuery('wars', getWars)
+}
