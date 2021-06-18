6b46fee5187eafdd445ca87282f816947476a109
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-18 22:48:29 +0530

    add react-query provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/home'
+
+const client = new QueryClient()
 
 const App = () => {
   return (
-    <ChakraProvider>
-      <Router>
-        <Switch>
-          <Route exact path='/'></Route>
-          <Route exact path='/roasters'></Route>
-          <Route exact path='/wars'></Route>
-        </Switch>
-      </Router>
-    </ChakraProvider>
+    <QueryClientProvider client={client}>
+      <ChakraProvider>
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/roaster'></Route>
+          </Switch>
+        </Router>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
