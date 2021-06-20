d4237460cb759dffa653be956316dd3139a2fde0
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-20 14:32:41 +0530

    remove chakra provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/home'
@@ -8,16 +7,14 @@ const client = new QueryClient()
 const App = () => {
   return (
     <QueryClientProvider client={client}>
-      <ChakraProvider>
-        <Router>
-          <Switch>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route exact path='/roaster'></Route>
-          </Switch>
-        </Router>
-      </ChakraProvider>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/roaster'></Route>
+        </Switch>
+      </Router>
     </QueryClientProvider>
   )
 }
