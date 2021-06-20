ae2fe19bf21e8340383c6fccce10290da17336bb
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-20 21:46:59 +0530

    add key

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <div className='grid p-5 gap-y-5'>
       <h2 className='text-3xl mb-4'>対戦一覧</h2>
-      {data?.map(w => (
-        <WarCard war={w} />
+      {data?.map(war => (
+        <WarCard key={war.id} war={war} />
       ))}
     </div>
   )
