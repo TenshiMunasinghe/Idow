2c3944ba0809ee7d3d973f74539507e59d9ba446
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-20 21:34:56 +0530

    make war card into single component

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,4 @@
-import CardWrapper from '../components/CardWrapper'
-import WarInfo from '../components/WarInfo'
+import WarCard from '../components/WarCard'
 import { useGetWars } from '../hooks/useGetWars'
 
 const Home = () => {
@@ -8,9 +7,7 @@ const Home = () => {
     <div className='grid p-5 gap-y-5'>
       <h2 className='text-3xl mb-4'>対戦一覧</h2>
       {data?.map(w => (
-        <CardWrapper key={w.id} to={`/war/${w.id}`}>
-          <WarInfo war={w} />
-        </CardWrapper>
+        <WarCard war={w} />
       ))}
     </div>
   )
