b438f7b090be1e7df811dbcc31b1da112958b6fe
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-20 14:33:01 +0530

    add id

--- client/src/hooks/useGetWars.ts
+++ client/src/hooks/useGetWars.ts
@@ -2,8 +2,9 @@ import ky from 'ky'
 import { useQuery } from 'react-query'
 import { War } from '../../../server/src/utils/get_players_details'
 
-interface ClientWar extends Omit<War, 'spin_time'> {
+export interface ClientWar extends Omit<War, 'spin_time'> {
   spin_time: string
+  id: string
 }
 
 const getWars: () => Promise<ClientWar[]> = async () => {
