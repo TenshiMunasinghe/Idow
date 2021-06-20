81eedcbab0ea5043139739a7536747919d47caa8
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-20 14:33:53 +0530

    show wars

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,12 +1,16 @@
-import { Box } from '@chakra-ui/react'
+import War from '../components/War'
 import { useGetWars } from '../hooks/useGetWars'
 
 const Home = () => {
   const { data } = useGetWars()
-  if (!data) return null
-  console.log(data.map(war => new Date(war.spin_time)))
-
-  return <Box>Hello Idow!</Box>
+  return (
+    <div className='grid p-5 gap-y-5'>
+      <h2 className='text-3xl mb-4'>対戦一覧</h2>
+      {data?.map(w => (
+        <War key={w.id} war={w} />
+      ))}
+    </div>
+  )
 }
 
 export default Home
