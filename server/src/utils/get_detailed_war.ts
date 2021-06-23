a1bcbdab9c81d8ba867466780a057498437fe859
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-23 16:55:31 +0530

    initial commit

---
+++ server/src/utils/get_detailed_war.ts
@@ -0,0 +1,40 @@
+import groupBy from 'lodash/groupBy'
+import { cocClient } from './coc_api'
+import { FormattedWar } from './format_war'
+
+export interface Player {
+  clan: {
+    name: string
+    tag: string
+  }
+  townHallLevel: number
+  tag: string
+  name: string
+}
+
+export interface Roaster {
+  [key: string]: Player[]
+}
+
+export interface DetailedWar extends Omit<FormattedWar, 'roaster'> {
+  roaster: Roaster
+}
+
+export const getDetailedRoaster = async (roaster: string[]) => {
+  const playersPromise: Promise<Player>[] = roaster.map(async (m: string) => {
+    const { clan, townHallLevel, tag, name } = await cocClient.playerByTag(m)
+    return {
+      clan: {
+        name: clan.name,
+        tag: clan.tag,
+      },
+      townHallLevel,
+      tag,
+      name,
+    } as Player
+  })
+
+  const players = await Promise.all(playersPromise)
+
+  return groupBy(players, 'townHallLevel') as Roaster
+}
