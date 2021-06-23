1da9a415c1004b816c7da912275d069d0ba36c31
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-23 23:44:00 +0530

    implement edit mode

--- client/src/pages/war.tsx
+++ client/src/pages/war.tsx
@@ -1,23 +1,44 @@
-import groupBy from 'lodash/groupBy'
-import { useMemo } from 'react'
+import { PencilIcon } from '@heroicons/react/solid'
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 import { useForm } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
-import { DetailedWar } from '../../../server/src/utils/get_players_details'
+import { DetailedWar } from '../../../server/src/utils/get_detailed_war'
 import FormGroup from '../components/FormGroup'
 import Input from '../components/Input'
+import Roaster from '../components/Roaster'
 import { useGetPlayers } from '../hooks/useGetPlayers'
 import { useGetWar } from '../hooks/useGetWar'
+import { dateToString } from '../utils/dateToString'
 
-interface FormData extends Omit<DetailedWar, 'spin_time'> {
+export interface FormData extends Omit<DetailedWar, 'spin_time' | 'roaster'> {
   spin_time: Date
 }
 
-const TH_COLORS: { [key: number]: string } = {
-  11: 'red',
-  12: 'cyan',
-  13: 'blue',
-  14: 'yellow',
+// like:
+//  {
+//   '11': ['#htrhst', ...],
+//   '12': ['fdgfgfa',...]
+//  }
+type RoasterTags = string[] | undefined
+
+interface Context {
+  isEditMode: boolean
+  roasterTags: RoasterTags
+  setRoasterTags: Dispatch<SetStateAction<RoasterTags>>
 }
+
+export const context = createContext<Context>({
+  isEditMode: false,
+  roasterTags: [],
+  setRoasterTags: () => {},
+})
 
 const War = () => {
   const { id } = useParams<{ id: string }>()
@@ -25,70 +46,75 @@ const War = () => {
   const players = useGetPlayers()
   const { register, handleSubmit } = useForm<FormData>()
 
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [roasterTags, setRoasterTags] = useState<RoasterTags>(war.data?.roaster)
+
+  useEffect(() => {
+    setRoasterTags(war.data?.roaster)
+  }, [war.data?.roaster])
+
   const isLoading = war.isLoading || players.isLoading
 
   const onSubmit = (values: FormData) => console.log(values)
 
-  // useEffect(() => {
-  //   console.log(data)
-  // }, [data])
-
-  const categorizedPlayers = useMemo(() => {
-    return groupBy(war.data?.roaster, 'townHallLevel')
-  }, [war.data?.roaster])
-
-  const townHalls = Object.keys(categorizedPlayers).sort(
-    (a, b) => parseInt(b) - parseInt(a)
+  const townHalls = useMemo(
+    () =>
+      players.data
+        ? Object.keys(players.data)
+            .map(th => parseInt(th))
+            .sort((a, b) => b - a)
+        : [],
+    [players.data]
   )
+
+  const toggleEditMode = () => setIsEditMode(prev => !prev)
 
   return (
     <div className='p-5'>
       {isLoading && <div>Loading...</div>}
       {!isLoading && !war.data && <div>無効なID</div>}
       {!isLoading && war.data && players.data && (
-        <>
-          <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+        <context.Provider value={{ isEditMode, roasterTags, setRoasterTags }}>
+          <div
+            onClick={toggleEditMode}
+            className={`bg-${isEditMode ? 'violet' : 'gray'}-700 text-${
+              isEditMode ? 'violet' : 'gray'
+            }-200 w-min mb-5 rounded-md ml-auto p-2`}>
+            <PencilIcon className='w-5 h-5' />
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
             <FormGroup>
               <label>対戦相手</label>
-              <Input register={register('opponent')} />
+              {isEditMode ? (
+                <Input
+                  register={register('opponent')}
+                  defaultValue={war.data.opponent}
+                />
+              ) : (
+                <span className='font-semibold'>{war.data.opponent}</span>
+              )}
             </FormGroup>
 
             <FormGroup>
               <label>マッチング時間</label>
-              <Input type='datetime-local' register={register('spin_time')} />
+              {isEditMode ? (
+                <Input type='datetime-local' register={register('spin_time')} />
+              ) : (
+                <span>{dateToString(new Date(war.data.spin_time))}</span>
+              )}
             </FormGroup>
 
-            <div className='grid gap-y-3'>
-              {players.data
-                .sort((a, b) => b.townHallLevel - a.townHallLevel)
-                .map(player => (
-                  <FormGroup key={player.tag}>
-                    <label>
-                      <span
-                        className={`text-${
-                          TH_COLORS[player.townHallLevel] || 'gray'
-                        }-300 mr-3`}>
-                        TH{player.townHallLevel}
-                      </span>
-                      {player.name}
-                    </label>
-                    <input
-                      type='radio'
-                      {...register('roaster')}
-                      name={player.name}
-                      value={player.tag}
-                    />
-                  </FormGroup>
-                ))}
-            </div>
+            <Roaster townHalls={townHalls} roaster={players.data} />
 
-            <button
-              type='submit'
-              className='w-full px-4 py-3 text-lg font-semibold bg-violet-900 rounded'>
-              決定
-            </button>
+            {isEditMode && (
+              <button
+                type='submit'
+                className='w-full px-4 py-3 text-lg font-semibold bg-violet-800 rounded-md'>
+                決定
+              </button>
+            )}
           </form>
-        </>
+        </context.Provider>
       )}
     </div>
   )
