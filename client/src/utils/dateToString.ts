730d860a90f7199aa94eb2e9f76e7377c9381d19
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-24 20:09:49 +0530

    fix bug

--- client/src/utils/dateToString.ts
+++ client/src/utils/dateToString.ts
@@ -1,5 +1,4 @@
 export const dateToString = (date: Date) =>
-  `${date.getMonth()}月${date.getDay()}日 ${String(date.getHours()).padStart(
-    2,
-    '0'
-  )}:${String(date.getMinutes()).padStart(2, '0')}`
+  `${date.getMonth() + 1}月${date.getDate()}日 ${String(
+    date.getHours()
+  ).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`
