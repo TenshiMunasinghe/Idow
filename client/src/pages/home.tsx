9b6290dc467faa650f1a14c13e4ac898fd24bcf0
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-24 21:53:29 +0530

    add link to new war

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,3 +1,5 @@
+import { DocumentIcon } from '@heroicons/react/solid'
+import { Link } from 'react-router-dom'
 import WarCard from '../components/WarCard'
 import { useGetWars } from '../hooks/useGetWars'
 
@@ -6,6 +8,12 @@ const Home = () => {
   return (
     <div className='grid p-5 gap-y-5'>
       <h2 className='text-3xl mb-4'>対戦一覧</h2>
+      <Link
+        to='/war/new'
+        className='flex space-x-2 bg-gray-700 rounded-md w-min ml-auto p-2'>
+        <span className='whitespace-nowrap'>新規対戦</span>
+        <DocumentIcon className='w-6 h-6' />
+      </Link>
       {data?.map(war => (
         <WarCard key={war.id} war={war} />
       ))}
