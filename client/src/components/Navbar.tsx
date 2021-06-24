ab246771c191df473722d5b15f1d75a4694d0a48
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-24 21:53:03 +0530

    increase horizontal padding

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='full-width bg-gray-800 p-3'>
+    <nav className='full-width bg-gray-800 py-3 px-5'>
       <Link to='/' className='font-extrabold text-xl'>
         Idow
       </Link>
