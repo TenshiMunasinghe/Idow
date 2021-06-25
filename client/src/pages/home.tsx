6d86cfc4eed3339616e4ae80dae6a7b12b1acf83
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-25 21:46:42 +0530

    align items inside link

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -5,10 +5,13 @@ import { useGetWars } from '../hooks/useGetWars'
 
 const Home = () => {
   const { data } = useGetWars()
+
   return (
     <div className='grid p-5 gap-y-5'>
       <h2 className='text-3xl mb-4'>対戦一覧</h2>
-      <Link to='/war/new' className='flex text-violet-200 w-min ml-auto'>
+      <Link
+        to='/war/new'
+        className='flex items-center justify-center space-x-1 text-violet-200 w-min ml-auto'>
         <span className='whitespace-nowrap'>新規対戦</span>
         <DocumentAddIcon className='w-5 h-5' />
       </Link>
