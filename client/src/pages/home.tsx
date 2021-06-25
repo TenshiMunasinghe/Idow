0a49b853111ffe7dabe0e3cd35afcd53ae39503d
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-25 15:49:21 +0530

    redesign link

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { DocumentIcon } from '@heroicons/react/solid'
-import Button from '../components/Button'
+import { Link } from 'react-router-dom'
 import WarCard from '../components/WarCard'
 import { useGetWars } from '../hooks/useGetWars'
 
@@ -8,10 +8,10 @@ const Home = () => {
   return (
     <div className='grid p-5 gap-y-5'>
       <h2 className='text-3xl mb-4'>対戦一覧</h2>
-      <Button as='link' to='/war/new' className='bg-gray-700 w-min ml-auto'>
+      <Link to='/war/new' className='flex text-violet-200 w-min ml-auto'>
         <span className='whitespace-nowrap'>新規対戦</span>
-        <DocumentIcon className='w-6 h-6' />
-      </Button>
+        <DocumentIcon className='w-5 h-5' />
+      </Link>
       {data?.map(war => (
         <WarCard key={war.id} war={war} />
       ))}
