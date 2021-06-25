61c8b4c5299f443cedf380b116b6a55ff7eaa2b3
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-25 21:22:17 +0530

    change icon

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { DocumentIcon } from '@heroicons/react/solid'
+import { DocumentAddIcon } from '@heroicons/react/solid'
 import { Link } from 'react-router-dom'
 import WarCard from '../components/WarCard'
 import { useGetWars } from '../hooks/useGetWars'
@@ -10,7 +10,7 @@ const Home = () => {
       <h2 className='text-3xl mb-4'>対戦一覧</h2>
       <Link to='/war/new' className='flex text-violet-200 w-min ml-auto'>
         <span className='whitespace-nowrap'>新規対戦</span>
-        <DocumentIcon className='w-5 h-5' />
+        <DocumentAddIcon className='w-5 h-5' />
       </Link>
       {data?.map(war => (
         <WarCard key={war.id} war={war} />
