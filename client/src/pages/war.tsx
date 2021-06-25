6f87855346294b3068d3e2501a7d1dac335e6ff0
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-25 17:32:29 +0530

    alert when spin_time is empty

--- client/src/pages/war.tsx
+++ client/src/pages/war.tsx
@@ -71,7 +71,12 @@ const War = () => {
   const onSubmit = useCallback(
     async (values: FormData) => {
       const spin_time = values.spin_time || war.data?.spin_time
-      if (!roasterTags || !spin_time) return
+      console.log(spin_time)
+
+      if (!roasterTags || !spin_time) {
+        alert('マッチング時間を入力してください')
+        return
+      }
       const data: Omit<FormattedWar, 'id'> = {
         ...values,
         roaster: roasterTags,
