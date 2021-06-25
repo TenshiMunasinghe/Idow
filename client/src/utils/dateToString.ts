8b046c551a74a49f575190c60e9254c9e876b737
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-25 21:46:26 +0530

    return empty string for invalid date

--- client/src/utils/dateToString.ts
+++ client/src/utils/dateToString.ts
@@ -1,4 +1,6 @@
 export const dateToString = (date: Date) =>
-  `${date.getMonth() + 1}月${date.getDate()}日 ${String(
-    date.getHours()
-  ).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`
+  isNaN(date.getTime())
+    ? ''
+    : `${date.getMonth() + 1}月${date.getDate()}日 ${String(
+        date.getHours()
+      ).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`
