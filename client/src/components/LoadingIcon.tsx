442108042347577f78c385ec7194b75d3b0ae77e
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-25 22:05:32 +0530

    initial commit

---
+++ client/src/components/LoadingIcon.tsx
@@ -0,0 +1,6 @@
+import { CubeTransparentIcon } from '@heroicons/react/solid'
+const LoadingIcon = () => {
+  return <CubeTransparentIcon className='w-7 h-7 animate-spin mx-auto' />
+}
+
+export default LoadingIcon
