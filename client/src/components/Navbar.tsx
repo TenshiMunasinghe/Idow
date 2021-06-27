9c47d1b63859597415f49cf11b85fefe0ea65478
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-27 23:27:23 +0530

    add media query

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='full-width bg-gray-800 py-3 px-5'>
-      <Link to='/' className='font-extrabold text-xl'>
+    <nav className='full-width bg-gray-800 py-3 px-5 lg:py-4 lg:px-6'>
+      <Link to='/' className='font-extrabold text-xl lg:text-2xl'>
         Idow
       </Link>
     </nav>
