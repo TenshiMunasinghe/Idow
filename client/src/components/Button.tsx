185394e5a3925981053e7eac284cd5d05f0f516b
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-27 15:58:27 +0530

    redesign button component

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,14 +1,25 @@
 import { ButtonHTMLAttributes, forwardRef } from 'react'
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+const SIZE_MAPS = {
+  lg: 'px-8 text-lg',
+  md: 'px-6 text-base',
+  sm: 'px-4 text-sm',
+}
+
+interface Props
+  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'className'> {
+  color: 'violet' | 'gray' | 'rose'
+  size: 'lg' | 'sm' | 'md'
+}
 
 const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => {
-  const { className, ...attrs } = props
+  const { color, size, ...attrs } = props
+
   return (
     <button
       {...attrs}
       ref={ref}
-      className={`flex items-center justify-center text-center space-x-2 px-4 py-3 font-semibold rounded-md ${className} focus:outline-none focus:ring-2 focus:ring-gray-400`}>
+      className={`inline-flex items-center justify-center text-center whitespace-nowrap py-3 font-semibold rounded-md focus:outline-none focus:ring-2 ${SIZE_MAPS[size]} bg-${color}-700 text-${color}-100 ring-${color}-400`}>
       {props.children}
     </button>
   )
