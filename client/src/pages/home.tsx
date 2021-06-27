49310033e5f5e1b1b2ded577fef111acf3af47b6
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-27 23:28:20 +0530

    isolate war list into a component

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,12 +1,8 @@
 import { DocumentAddIcon } from '@heroicons/react/solid'
 import { Link } from 'react-router-dom'
-import LoadingIcon from '../components/LoadingIcon'
-import WarCard from '../components/WarCard'
-import { useGetWars } from '../hooks/useGetWars'
+import WarList from '../components/WarList'
 
 const Home = () => {
-  const { data, isLoading } = useGetWars()
-
   return (
     <div className='grid p-5 gap-y-5'>
       <h2 className='text-2xl mb-2 font-bold'>対戦一覧</h2>
@@ -16,8 +12,7 @@ const Home = () => {
         <span className='whitespace-nowrap'>新規対戦</span>
         <DocumentAddIcon className='w-5 h-5' />
       </Link>
-      {isLoading && <LoadingIcon />}
-      {!isLoading && data?.map(war => <WarCard key={war.id} war={war} />)}
+      <WarList className='grid lg:grid-cols-2' />
     </div>
   )
 }
