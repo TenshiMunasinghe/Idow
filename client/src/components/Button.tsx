58dfb16814ef5ce81d4839998e3e88430b8f6390
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-28 13:45:07 +0530

    remove interpolation of tw classes

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -6,10 +6,16 @@ const SIZE_MAPS = {
   sm: 'px-4 text-sm',
 }
 
+const COLOR_MAPS = {
+  violet: 'bg-violet-700 text-violet-100 ring-violet-400',
+  gray: 'bg-gray-700 text-gray-100 ring-gray-400',
+  rose: 'bg-rose-700 text-rose-100 ring-rose-400',
+}
+
 interface Props
   extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'className'> {
-  color: 'violet' | 'gray' | 'rose'
-  size: 'lg' | 'sm' | 'md'
+  size: keyof typeof SIZE_MAPS
+  color: keyof typeof COLOR_MAPS
 }
 
 const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => {
@@ -19,7 +25,7 @@ const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => {
     <button
       {...attrs}
       ref={ref}
-      className={`inline-flex items-center justify-center text-center whitespace-nowrap py-3 font-semibold rounded-md focus:outline-none focus:ring-2 ${SIZE_MAPS[size]} bg-${color}-700 text-${color}-100 ring-${color}-400`}>
+      className={`inline-flex items-center justify-center text-center whitespace-nowrap py-3 font-semibold rounded-md focus:outline-none focus:ring-2 ${SIZE_MAPS[size]} ${COLOR_MAPS[color]}`}>
       {props.children}
     </button>
   )
