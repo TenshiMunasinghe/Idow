e50ffb04e2ddb1225d890e5c99d42ffb4d19bdea
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-28 15:42:13 +0530

    remove margin

--- client/src/components/LoadingIcon.tsx
+++ client/src/components/LoadingIcon.tsx
@@ -1,6 +1,6 @@
 import { CubeTransparentIcon } from '@heroicons/react/solid'
 const LoadingIcon = () => {
-  return <CubeTransparentIcon className='w-7 h-7 animate-spin mx-auto' />
+  return <CubeTransparentIcon className='w-7 h-7 animate-spin' />
 }
 
 export default LoadingIcon
