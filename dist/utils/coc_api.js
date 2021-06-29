c1ed7e92b4c832f469fadba0c21419cc3dda91f5
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-30 02:15:53 +0530

    add dist files

---
+++ dist/utils/coc_api.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cocClient = void 0;
+var config_1 = require("./config");
+var clashApi = require('clash-of-clans-api');
+exports.cocClient = clashApi({
+    token: config_1.parsed === null || config_1.parsed === void 0 ? void 0 : config_1.parsed.COC_API_TOKEN,
+});
