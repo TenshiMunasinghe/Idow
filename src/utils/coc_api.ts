60f1c12ca92a20a32dae4633057a00939940d265
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-30 18:52:20 +0530

    add proxy on prod

--- src/utils/coc_api.ts
+++ src/utils/coc_api.ts
@@ -2,9 +2,14 @@ import { parsed } from './config'
 
 const clashApi = require('clash-of-clans-api')
 
-export const cocClient = clashApi({
-  token:
-    process.env.NODE_ENV === 'production'
-      ? process.env.COC_API_TOKEN_PROD
-      : parsed?.COC_API_TOKEN_TEST,
-})
+const options =
+  process.env.NODE_ENV === 'production'
+    ? {
+        token: process.env.COC_API_TOKEN_PROD,
+        request: {
+          proxy: process.env.FIXIE_URL,
+        },
+      }
+    : { token: parsed?.COC_API_TOKEN_TEST }
+
+export const cocClient = clashApi(options)
