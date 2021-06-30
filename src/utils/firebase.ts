ae25bfaef1bf6dc507c7b83e0e81f7fee0bf1631
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-06-30 13:18:42 +0530

    switch db in dev and prod

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,8 +4,11 @@ const serviceAccount = require('../../config/firebase-test.json')
 
 export type TimeStamp = firebase.firestore.Timestamp
 
+const environment =
+  process.env.NODE_ENV === 'production' ? 'production' : 'test'
+
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
+  credential: firebase.credential.cert(serviceAccount[environment]),
 })
 
 export const db = firebase.firestore()
