925f34a4d6156532cd7ff5d391a89b882d7f2f4b
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-07-02 13:20:23 +0530

    generated

--- dist/utils/coc_api.js
+++ dist/utils/coc_api.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.cocClient = void 0;
 var config_1 = require("./config");
 var clashApi = require('clash-of-clans-api');
+console.log(process.env.NODE_ENV);
 var options = process.env.NODE_ENV === 'production'
     ? {
         token: process.env.COC_API_TOKEN_PROD,
